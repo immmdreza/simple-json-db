d63faef7c098bbfc164ec76b7b97ff51f7aa0e81
immmdreza <ir310022@gmail.com>
2022-05-27 20:37:21 +0430

    More examples

---
+++ src/examples/queryable_1.py
@@ -0,0 +1,53 @@
+import asyncio
+
+from sjd import Engine, Collection, TEntity, properties
+
+
+@properties.collect_props_from_init
+class Student(TEntity):
+    def __init__(self, first_name: str) -> None:
+        self.first_name = first_name
+
+
+class StudentsCollection(Collection[Student]):
+    def __init__(self, engine: Engine) -> None:
+        super().__init__(engine, Student, "studentsCollection")
+
+    async def add_student(self, first_name: str):
+        return await self.add(Student(first_name))
+
+
+class AppEngine(Engine):
+
+    students = Engine.typed_set(Student, StudentsCollection)
+
+    def __init__(self):
+        super().__init__("app_engine")
+
+
+async def main():
+
+    engine = AppEngine()
+
+    await engine.students.add(Student("John"))
+    await engine.students.add(Student("Johnny"))
+    await engine.students.add(Student("Jill"))
+    await engine.students.add(Student("Will"))
+    await engine.students.add(Student("Bill"))
+
+    async with engine.students.get_queryable() as students:
+        async for student in students.where(lambda s: s.first_name.startswith("J")):
+            print(student.first_name)
+
+            # John
+            # Johnny
+            # Jill
+
+        # Prev filter is applied yet
+        all_are_j = await students.all(lambda s: s.first_name.startswith("J"))
+        print(all_are_j)
+        # True
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ src/examples/typed_collection.py
@@ -0,0 +1,50 @@
+import asyncio
+from sjd import Engine, Collection, TEntity, properties as props
+
+
+@props.collect_props_from_init
+class Employee(TEntity):
+    def __init__(self, employee_id: int, first_name: str, last_name: str):
+        self.employee_id = employee_id
+        self.first_name = first_name
+        self.last_name = last_name
+
+
+class EmployeeCollection(Collection[Employee]):
+    def __init__(self, engine: "Engine", /) -> None:
+        super().__init__(engine, Employee, "employeesCollection")
+
+    async def add_employee(self, first_name: str, last_name: str) -> None:
+        added_count = await self.count()
+        employee_id = 100000 + added_count
+        await self.add(Employee(employee_id, first_name, last_name))
+
+
+class AppEngine(Engine):
+
+    employees = Engine.typed_set(Employee, EmployeeCollection)
+
+    def __init__(self):
+        super().__init__("__test_db__")
+
+
+async def main():
+    engine = AppEngine()
+    employees_col = engine.employees
+
+    await employees_col.add_employee("John", "Smite")
+    await employees_col.add_employee("Tommy", "Smite")
+    await employees_col.add_employee("Sara", "Smite")
+    await employees_col.add_employee("Andy", "Smite")
+
+    async for employee in employees_col:
+        print(employee.employee_id)
+
+    # 100000
+    # 100001
+    # 100002
+    # 100003
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
